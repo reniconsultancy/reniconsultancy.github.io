a692ea58f692a8261670dada5d3bcf4a3853f466
Mackenzie Clark <clark.rmackenzie@gmail.com>
2021-01-25 17:40:32 -0500

    two column layout styles

--- reniconsultancyllc/src/App.js
+++ reniconsultancyllc/src/App.js
@@ -1,3 +1,5 @@
+import './App.css';
+
 import {
   BrowserRouter as Router,
   Switch,
--- reniconsultancyllc/src/pages/Home.js
+++ reniconsultancyllc/src/pages/Home.js
@@ -1,7 +1,15 @@
 function Home() {
     return (
-      <div className="container">
-        <div>Hello</div>
+      <div>
+        <div className="container">
+          <div>Hello</div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="column">Hello</div>
+            <div className="column">There</div>
+          </div>
+        </div>
       </div>
     );
   }
