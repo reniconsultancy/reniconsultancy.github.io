47b1cb261756ae27a0087d0b49f1c14773b76572
Mackenzie Clark <clark.rmackenzie@gmail.com>
2021-01-26 16:08:32 -0500

    styling and content for goals in about section, responsive sizing at viewport 900px

--- reniconsultancyllc/src/pages/About.js
+++ reniconsultancyllc/src/pages/About.js
@@ -1,6 +1,35 @@
 function About() {
     return (
-      <div></div>
+      <div>
+        
+        <div className="container navy">
+          <div><strong>At RENI, we value community partnership above all else.</strong><br/><br/> We look forward to connecting with you and working together to elevate the quality of work you do to best serve your communities. We do not want cost to deter you from utilizing RENI’s services. We offer a free consultation to first determine what your needs are and then work with you to come to an agreement on what consultation fees you and your organization are comfortable with.</div>
+        </div>
+        
+        
+        <div className="container goals">
+          <div>With over 15 years of experience, RENI’s main goals when working with agency partners include:</div>
+
+          <div><strong>Goal 1:</strong> To identify the roadmap through which agencies can align their services to achieve their goals effectively. <br/><br/> <em>This includes: logic models, outcomes, strategic planning, program development, program implementation</em></div>
+
+          <div><strong>Goal 2:</strong> To enhance the quality of services provided by agencies to clients and consumers through ongoing evaluation and quality control. <br/><br/> <em>This includes: long term program evaluation, short term evaluation, passive evaluation, satisfaction surveys, quality assurance</em></div>
+
+          <div><strong>Goal 3:</strong> To create ongoing partnerships through networking and collaboration to uplift community voices and advance client centered practices. <br/><br/> <em>This includes: Identifying research based best practices linked to efforts; connecting community stakeholders for potential collaboration; utilizing CBPR strategies</em></div>
+        </div>
+        
+        <div className="container orange">
+          <div></div>
+        </div>
+        
+        <div className="container beige">
+          <div></div>
+        </div>
+        
+        <div className="container">
+          <div></div>
+        </div>
+
+      </div>
     );
   }
   
