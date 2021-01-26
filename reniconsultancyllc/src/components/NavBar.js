a48e0fa1ca6a1d11c423195e56ac204fe7f466a1
Mackenzie Clark <clark.rmackenzie@gmail.com>
2021-01-26 00:22:33 -0500

    navagation bar linked with react-router-dom LINK

--- reniconsultancyllc/src/components/NavBar.js
+++ reniconsultancyllc/src/components/NavBar.js
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom";
+
 function NavBar() {
     return (
-      <div>hi</div>
+      <div id="navbar"> 
+        <ul> 
+        <li><Link to="/">Home</Link></li> 
+        <li><Link to="/about">About</Link></li> 
+        <li><Link to="/services">Services</Link></li> 
+        <li><Link to="/mentorship">Mentorship</Link></li> 
+        <li><Link to="/contact">Contact</Link></li> 
+        </ul> 
+      </div> 
     );
   }
   
