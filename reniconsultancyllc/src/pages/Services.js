f9892d12438879c8a0c6ada3b8b89030886b2cb7
Mackenzie Clark <clark.rmackenzie@gmail.com>
2021-01-27 03:31:10 -0500

    services page container layout

--- reniconsultancyllc/src/pages/Services.js
+++ reniconsultancyllc/src/pages/Services.js
@@ -1,6 +1,27 @@
 function Services() {
     return (
-      <div></div>
+      <div>
+
+        <div className="row">
+            <div className="column">
+              <div className="container">
+                RENI
+              </div>
+            </div>
+            <div className="column beige">
+              <div className="container">
+                <strong>RENI, (Research, Evaluation, and Networking Initiative)</strong> is an independent organization that aims to bridge the gap between research and practice, funding and evaluation, and clients and service providers.
+              </div>
+            </div>
+          </div>
+
+          <div className="container navy"></div>
+
+          <div className="container orange"></div>
+
+          <div className="container"></div>
+
+      </div>
     );
   }
   
