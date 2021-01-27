763215a58d151957604f54d63c4580ab5c9c5912
Mackenzie Clark <clark.rmackenzie@gmail.com>
2021-01-26 19:28:37 -0500

    content for about page

--- reniconsultancyllc/src/pages/About.js
+++ reniconsultancyllc/src/pages/About.js
@@ -6,7 +6,6 @@ function About() {
           <div><strong>At RENI, we value community partnership above all else.</strong><br/><br/> We look forward to connecting with you and working together to elevate the quality of work you do to best serve your communities. We do not want cost to deter you from utilizing RENI’s services. We offer a free consultation to first determine what your needs are and then work with you to come to an agreement on what consultation fees you and your organization are comfortable with.</div>
         </div>
         
-        
         <div className="container goals">
           <div>With over 15 years of experience, RENI’s main goals when working with agency partners include:</div>
 
@@ -18,17 +17,15 @@ function About() {
         </div>
         
         <div className="container orange">
-          <div></div>
+          <div>Roxanna Ast</div>
+          <div>Roxanna is a Doctoral candidate in Social Work and holds a Master of Social Work degree and Master of Psychological Studies degree. Since 2009, Roxanna has worked as a direct service provider in various non-profits around the world including in the Middle East, United Kingdom, and most recently the United States. As a service provider, Roxanna has worked closely with survivors of violent crimes, immigrant and refugee communities, and people living with HIV/AIDS. As of 2013, Roxanna has been involved in design, development, implementation, and evaluation for various programs funded by Federal, State, and local funders.</div>
         </div>
         
         <div className="container beige">
-          <div></div>
+          <div>Jennifer Schrum</div>
+          <div>Jenny is a Doctoral student and Master’s student in Social Work and holds a Master of Public Health degree. She has worked with a variety of non-profit organizations in the Washington, DC and NY/NJ areas delivering and advising direct service programs related to nutrition, PreK-12 education, substance use, and basic needs. Since 2012, Jenny has supported non-profit organization efforts through strategic planning, program design/implementation, and program evaluation.</div>
         </div>
         
-        <div className="container">
-          <div></div>
-        </div>
-
       </div>
     );
   }
