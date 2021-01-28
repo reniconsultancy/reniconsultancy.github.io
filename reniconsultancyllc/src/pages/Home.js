4e4e5d650e38f7a2ea9b63d76af38760ed1c3bca
Mackenzie Clark <clark.rmackenzie@gmail.com>
2021-01-28 01:06:04 -0500

    responsivegit add .git add .

--- reniconsultancyllc/src/pages/Home.js
+++ reniconsultancyllc/src/pages/Home.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Teamwork from "../assets/teamwork.png";
 import Ideas from "../assets/ideas.png";
 
@@ -18,17 +19,19 @@ function Home() {
           </div>
   
         <div className="container navy">
-          <div><strong>RENI provides consultation on program design, implementation, and evaluation to community service providers (non-profits, counseling centers, community corporations, etc.) to establish research based practices and enhance client centered goals.</strong></div> 
           <div>Although many organizations have begun using statistics few have enough data to guide planning, management systems, or redirect and redesign underperforming programs or services. The real data from National Center on Charitable Statistics reveals that approximately <strong>30% of nonprofits fail</strong> to exist after 10 years, and according to Forbes, over half of all nonprofits that are chartered are destined to fail or stall within a few years due to leadership issues, lack of a strategic plan, lack of relevant and accessible services, among other things.</div>
         </div>
 
         <div className="container">
           <div className="row">
-            <div className="column">
+            <div className="column disappear">
               <img src={Ideas} />
             </div>
             <div className="column">
-              <div>At RENI, we rely on community partnership and shared expertise to help organizations and agencies reach their goals through effective strategic planning, program development and implementation, and evaluations.</div>
+              <div>RENI provides consultation on program design, implementation, and evaluation to community service providers (non-profits, counseling centers, community corporations, etc.) to establish research based practices and enhance client centered goals.</div>
+              <div style={{paddingTop: "40px"}}>
+              <button className="orange"><Link to="/contact">Connect with us.</Link></button>
+              </div>
             </div>
           </div>
         </div>
--- reniconsultancyllc/src/pages/Services.js
+++ reniconsultancyllc/src/pages/Services.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Connectivity from "../assets/connectivity.png";
 import Analysis from "../assets/analysis.png";
 
@@ -18,9 +19,9 @@ function Services() {
             </div>
           </div>
 
-          <div className="container navy">
+          <div className="container navy disHeight">
             <div className="row">Programming</div>
-            <div className="row smallFont">
+            <div className="row smallFont extraTextHide">
             Logic models – A logic model is a visual map depicting the relationship between your program’s resources and activities and the intended outputs and outcomes of your efforts. <br/> <br/>
             Outcomes – Outcomes are the desired results of a program’s efforts and include learning, action, and conditions. Selecting realistic and purposeful outcomes is essential to positively impacting people and communities. <br/> <br/>
             Strategic planning – Strategic planning establishes the purpose and direction of your organization and programs to meet client needs with appropriate and sustainable efforts. This includes appropriately communicating the mission, values, strengths/weaknesses, strategies, resources, and ongoing evaluation plan. <br/> <br/>
@@ -28,9 +29,9 @@ function Services() {
             Program implementation – Program implementation includes the management and ongoing evaluation of efforts. Implementation strategies involve defining processes and procedures to organize the work and track efforts linked to outcomes.</div>
           </div>
 
-          <div className="container orange">
-            <div className="row">Evaluation</div>
-            <div className="row smallFont">
+          <div className="container orange disHeight">
+            <div className="row hide800">Evaluation</div>
+            <div className="row smallFont extraTextHide">
             Long-term evaluation - Long-term program evaluation includes evaluation of programs and workshops that are offered on an ongoing basis such as a 12-week psychosocial support group or educational groups. This type of evaluation can offer insight into the influence of programs over time and inform the ways in which programs can be adjusted and improved. <br/> <br/>
             Short-term evaluation – Short-term evaluation is most commonly utilized to evaluate programs and workshops that are offered in one session. These evaluations can be conducted in person or virtually and often provide insightful information on the most significant and impactful aspects of these programs to improve the quality for future programs. <br/> <br/>
             Passive evaluation—Passive evaluation is used to evaluate programs that are delivered in nontraditional ways such as public service announcements, art installations, messaging boards, and pop up workshops. These evaluations are useful to enhance community awareness, community participation, and advocacy efforts.<br/> <br/>
@@ -42,11 +43,14 @@ function Services() {
 
           <div className="container">
             <div className="row">
-              <div className="column">
+              <div className="column disappear">
                 <img src={Analysis} />
               </div>
               <div className="column">
-                <div>At RENI, we rely on community partnership and shared expertise to help organizations and agencies reach their goals through effective strategic planning, program development and implementation, and evaluations.</div>
+                <div>Whether you are a one-person, private practice or a large agency, we can help you make data-driven decisions to execute best practices and achieve planned success.</div>
+                <div style={{paddingTop: "40px"}}>
+                <button className="orange"><Link to="/contact">Connect with us.</Link></button>
+              </div>
               </div>
             </div>
           </div>
