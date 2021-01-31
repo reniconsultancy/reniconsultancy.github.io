265f9b78dcc47343fa481e25ca29479d307cef83
Mackenzie Clark <clark.rmackenzie@gmail.com>
2021-01-31 03:14:10 -0500

    color changes

--- src/pages/Mentorship.js
+++ src/pages/Mentorship.js
@@ -3,7 +3,7 @@ function Mentorship() {
       <div>
         <div className="container">
           <div> <strong style={{color: "#ad6c3e"}}>We are working on something fantastic!</strong> <br/> <br/>
-          Follow us on <a href="https://twitter.com/RENILLC" style={{color: "#bd9e82"}}>Twitter</a> for updates on our Mentorship Program.</div>
+          Please continue to follow us for updates on our Mentorship Program.</div>
         </div>
       </div>
     );
